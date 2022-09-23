29fcd1fa188d13400f8473cd1e90fe142e8fb0ec
D. Fyffe <fyffed@netzero.net>
2022-09-23 17:42:03 -0400

    Genesis steering alert  fix 2

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -738,8 +738,8 @@ class Controls:
       if len(dpath_points):
         # Check if we deviated from the path
         # TODO use desired vs actual curvature
-        left_deviation = actuators.steer > 0 and dpath_points[0] < -0.35
-        right_deviation = actuators.steer < 0 and dpath_points[0] > 0.35
+        left_deviation = actuators.steer > 0 and dpath_points[0] < -0.45
+        right_deviation = actuators.steer < 0 and dpath_points[0] > 0.45
 
         if left_deviation or right_deviation:
           self.events.add(EventName.steerSaturated)
