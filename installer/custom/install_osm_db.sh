a767eba28ea50082af3f5270c8c025f072e80fc6
mrdonone <114766462+mrdonone@users.noreply.github.com>
2022-10-01 01:12:59 -0400

    OSM offline Georgia 1

--- installer/custom/install_osm_db.sh
+++ installer/custom/install_osm_db.sh
@@ -4,22 +4,24 @@ export GZ_MAP_FILE_NAME=db.tar.gz
 export GZ_MAP_FILE=${OSM_DIR}/${GZ_MAP_FILE_NAME}
 
 # Remove legacy compressed map file if existing
-[ -f $GZ_MAP_FILE ] && rm -rf $GZ_MAP_FILE
+rm -rf $GZ_MAP_FILE
 
 # WD
 cd $OSM_DIR
 
 # Download map file
-wget https://opkr.tk/osm/db/kor/${GZ_MAP_FILE_NAME}
+#wget https://opkr.tk/osm/db/kor/${GZ_MAP_FILE_NAME}
 
-if [ "$?" != 0 ]; then
-  echo "Error downloading map file"
-else
+#if [ "$?" != 0 ]; then
+  #echo "Error downloading map file"
+#else
   echo "Successfuly downloaded map file"
   # Remove current db dir if exisiting
-  [ -f $DB_DIR ] && rm -rf $DB_DIR
+  rm -rf $DB_DIR
   # Uncompress
-  tar -zxvf db.tar.gz
+  #tar -zxvf db.tar.gz
+  sudo apt -y install osmctools
+  osmconvert --out-osm georgia-latest.osm.pbf | ~/osm/osm-3s_v0.7.56/bin/update_database --meta --db-dir="/home/data/osm/db/"
   # Remove compressed map files after expanding
-  [ -f $GZ_MAP_FILE ] && rm -rf $GZ_MAP_FILE
+  rm -rf $GZ_MAP_FILE
 fi
